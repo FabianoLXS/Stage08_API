1b47e636841252b9fd83ff2139671e69cfd4d0c9
FabianoLXS <fab_luiz@msn.com>
2023-07-13 11:10:52 -0300

    commit

--- src/server.js
+++ src/server.js
@@ -1,7 +1,13 @@
-const express = require('express');
+const express = require("express")
 
-const app = express();
+const app = express()
 
-const PORT = 3333;
+app.get("/message/:id/:user", (request, response) => {
+  const {id, user} = request.params
+  response.send(`Id da mensagem: ${id}.
+  Para o usuário ${user}.`)
+})
 
-app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
+const PORT = 3333
+
+app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
