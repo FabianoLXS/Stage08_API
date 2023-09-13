adaadf4d16d813b0400020e518d6ee5129b0d99e
FabianoLXS <fab_luiz@msn.com>
2023-09-13 11:31:06 -0300

    commit

--- src/routes/sessions.routes.js
+++ src/routes/sessions.routes.js
@@ -1,10 +1,10 @@
 const { Router } = require("express")
 
 const SessionsController = require("../controllers/SessionsController")
-const SessionsController = require("../controllers/SessionsController")
-const SessionsController = new SessionsController()
+
+const sessionsController = new SessionsController()
 
 const sessionsRoutes = Router()
-sessionsRoutes.post("/", SessionsController.create)
+sessionsRoutes.post("/", sessionsController.create)
 
 module.exports = sessionsRoutes
